d756c0539f26e2623e5757d2bf30b174cb258e65
Dovavivavi <szilagyiabel1@gmail.com>
2022-08-24 08:42:10 +0200

    added fetch(not working)

--- frontend/fetch.js
+++
@@ -1,9 +0,0 @@
-const baseUrl = 'https://api.nasa.gov/planetary';
-const key = '4hrbRcDLDQtmrglPbHKC4ZYeZdrPL2jTbuBBDuC1';
-
-async function getApod() {
-  const response = await fetch(`${baseUrl}/apod?api_key=${key}`);
-  return response.json;
-}
-
-export default getApod;
--- frontend/script.js
+++ frontend/script.js
@@ -1,1 +1,23 @@
-import { getApod } from './fetch.js';
+const displayField = function () {
+  return /*html*/ `
+  <div class="img-field">
+  <img class="daily-img" src="" alt="">
+  </div>
+  <div class="text-field">
+  <p class="text">hello</p>
+  </div>
+  `;
+};
+
+async function fetchApod() {
+  const key = '4hrbRcDLDQtmrglPbHKC4ZYeZdrPL2jTbuBBDuC1';
+  let response = await fetch(
+    `https://api.nasa.gov/planetary/apod?api_key=${key}`
+  );
+  console.log(response);
+  let data = response.json();
+  console.log(data);
+}
+
+const root = document.querySelector('#root');
+root.insertAdjacentHTML('beforeend', displayField());
