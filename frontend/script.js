d48e184b12b85149d584451f6e949cfb103c4958
Dovavivavi <szilagyiabel1@gmail.com>
2022-08-26 09:36:55 +0200

    edited js and css

--- frontend/script.js
+++ frontend/script.js
@@ -2,8 +2,10 @@ const displayField = function () {
   return /*html*/ `
   <section id="current-image">
     <div class="menu-field">
+      <button class="hat">hatra</button>
       <input type="date" class="date-picker">
-      <button id="gallery-btn">Gallery</button>  
+      <button class="gallery-btn">Gallery</button>  
+      <button class="">elore</button>
     </div>
     <div class="img-container">
       <div class="img"></div>
@@ -23,8 +25,8 @@ async function fetchApod() {
   let key = '4hrbRcDLDQtmrglPbHKC4ZYeZdrPL2jTbuBBDuC1';
   let response = await fetch(
     `https://api.nasa.gov/planetary/apod?api_key=${key}`
+    // https://api.nasa.gov/planetary/apod?date=2022-08-12&api_key=${key}
   );
-  console.log(response);
   let data = await response.json();
   console.log(data);
   useData(data);
@@ -37,5 +39,23 @@ function useData(data) {
   ).innerHTML += `<img class="nasa-img" src="${data.url}">`;
 }
 
-const galleryButton = document.querySelector('#gallery-btn');
+const galleryButton = document.querySelector('.gallery-btn');
 galleryButton.addEventListener('click');
+
+// let date = null;
+// const dateField = document.querySelector('.date-picker');
+// function dateChange() {
+//   dateValue = dateField.value;
+//   date = dateValue;
+// }
+
+// dateField.addEventListener('input', dateChange());
+// console.log(dateChange());
+// console.log(date);
+
+const datePicker = document.querySelector('.date-picker');
+
+datePicker.addEventListener('change', (event) => {
+  const result = event.String();
+  console.log(result);
+});
